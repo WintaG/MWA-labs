58f3bdaf07095fe85548af2d4c33a05fd26fc9ca
WintaG <winta.gassefaw@gmail.com>
2018-10-07 15:27:37 -0500

    Update index.js

--- lab-5/express-gen/routes/index.js
+++ lab-5/express-gen/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Edw' });
+  res.render('index', { title: 'Winta' });
 });
 
 module.exports = router;
